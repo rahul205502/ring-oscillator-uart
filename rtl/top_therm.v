7d99faeccbb5b771da2873fe55e9414d06c2238e
Rahul M <rahul735857@gmail.com>
2026-01-02 11:46:56 +0530

    Refactor top_therm module to include new inputs
    Refactor top_therm module to include new inputs

--- rtl/top_therm.v
+++ rtl/top_therm.v
@@ -1,6 +1,6 @@
 `timescale 1ns/1ps
 
-module top_therm( input clk, input rst_n, output tx);//wiring module
+module top_therm (input clk, input rst, input en, input preset_en, input [7:0] preset_val, output tx);//wiring module
 
     reg start_pending; // start signal latch
     
@@ -10,25 +10,25 @@ module top_therm( input clk, input rst_n, output tx);//wiring module
     wire [7:0]latch_count;
     wire uart_busy;
     
-    always @(posedge clk or negedge rst_n) begin
-        if (!rst_n) begin
-            start_pending <= 1'b0;
+    always @(posedge clk or posedge en) begin
+        if (en) begin
+           if (window_done) begin
+               start_pending <= 1'b1;
+           end
+           else if(uart_busy) begin
+               start_pending <= 1'b0;
+           end 
         end
         else begin
-            if (window_done) begin
-                start_pending <= 1'b1;
-            end
-            else if(uart_busy) begin
-                start_pending <= 1'b0;
-            end
+            start_pending <= 1'b0;
         end
     end
          
-    ring_oscillator R1(.op(op) );
-    FF2 F1 (.clk(clk), .rst_n(rst_n) , .ro_in(op), .ro_out(ro_out) );
-    edge_detector ED1( .clk(clk), .rst_n(rst_n), .ro_out(ro_out), .edge_dec(edge_dec) );
-    edge_counter EC1( .clk(clk), .rst_n(rst_n), .edge_chk(edge_dec), .count(count) );
-    window w1 (.clk(clk), .rst_n(rst_n), .window_done(window_done) );
-    latch_register LR1 (.clk(clk), .rst_n(rst_n), .count(count), .window_done(window_done), .latch_count(latch_count) );
-    UART U1 (.clk(clk), .rst_n(rst_n), .start(start_pending), .latch_count(latch_count), .tx(tx), .tx_busy(uart_busy) );
+    ring_oscillator R1 (.en(en), .op(op) );
+    FF2 F1 (.clk(clk), .en(en) , .ro_in(op), .ro_out(ro_out) );
+    edge_detector ED1( .clk(clk), .en(en), .ro_out(ro_out), .edge_dec(edge_dec) );
+    edge_counter EC1( .clk(clk), .en(en), .edge_chk(edge_dec), .count(count) );
+    window w1 (.clk(clk), .en(en), .window_done(window_done) );
+    latch_register LR1 (.clk(clk), .en(en), .count(count), .window_done(window_done), .latch_count(latch_count) );
+    UART U1 (.clk(clk), .rst(rst), .en(en), .preset_en(preset_en), .preset_val(preset_val), .start(start_pending), .latch_count(latch_count), .tx(tx), .tx_busy(uart_busy) );
 endmodule
