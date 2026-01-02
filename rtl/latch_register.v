49aee77293fe078672624deaa55a6606a6e7b198
Rahul M <rahul735857@gmail.com>
2026-01-02 11:39:53 +0530

    Change latch_register to use enable instead of reset
    Modified latch_register to use enable signal instead of reset.

--- rtl/latch_register.v
+++ rtl/latch_register.v
@@ -1,15 +1,15 @@
 `timescale 1ns/1ps
 
-module latch_register ( input window_done, input [7:0]count, input clk, input rst_n, output reg [7:0] latch_count);
+module latch_register ( input window_done, input [7:0]count, input clk, input en, output reg [7:0] latch_count);
 
-    always @(posedge clk or negedge rst_n) begin 
-        if(!rst_n) begin
-            latch_count <= 8'b0;
+    always @(posedge clk or posedge en) begin 
+        if(en) begin
+           if (window_done) begin
+               latch_count <= count;
+           end 
         end
         else begin 
-            if (window_done) begin
-                latch_count <= count;
-            end
+            latch_count <= 8'b0;
         end
     end
     
