4275e798cb7ee080d11612c095b4ea314c8e8889
Rahul M <rahul735857@gmail.com>
2026-01-02 11:34:06 +0530

    Refactor edge_counter to use enable signal
    Modified edge_counter module to include enable signal and reset behavior.

--- rtl/edge_counter.v
+++ rtl/edge_counter.v
@@ -1,14 +1,14 @@
 `timescale 1ns/1ps
-module edge_counter ( input clk, input rst_n, input edge_chk, output reg [7:0]count );
+module edge_counter ( input clk, input en, input edge_chk, output reg [7:0]count );
 
-    always @ (posedge clk or negedge rst_n ) begin 
-     if (!rst_n) begin
-        count <= 8'b0;
-     end
-     else begin
+    always @ (posedge clk or posedge en) begin 
+     if (en) begin
         if(edge_chk) begin
             count <= count + 1'b1;
         end
+     end
+     else begin
+        count <= 8'b0;
       end
       end
       
