3bb05506dbe03f41087a7d18ac05054518feda00
Rahul M <rahul735857@gmail.com>
2026-01-04 13:28:38 +0530

    Add enable signal to edge_detector module
    Modified edge_detector module to include enable signal and reset behavior.

--- rtl/edge_detector.v
+++ rtl/edge_detector.v
@@ -1,16 +1,16 @@
 `timescale 1ns/1ps
 
-module edge_detector ( input clk, input ro_out,input rst_n, output edge_dec );
+module edge_detector ( input clk, input ro_out, input en, output edge_dec );
 
     reg ro_out_prev;
-    always @(posedge clk or negedge rst_n) begin
-        if (!rst_n) begin 
-            ro_out_prev <= 1'b0;
+    always @(posedge clk) begin
+        if (en) begin 
+            ro_out_prev <= ro_out;
         end
         else begin 
-            ro_out_prev <= ro_out ;
+            ro_out_prev <= 1'b0;
         end
      end
      
-     assign edge_dec = (~ro_out_prev) & ro_out ;
+     assign edge_dec = (~ro_out_prev) & ro_out;
 endmodule
