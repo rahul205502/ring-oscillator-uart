1748ab44c579443b5cd2f349e351136e155390db
Rahul M <rahul735857@gmail.com>
2026-01-04 13:27:40 +0530

    Add enable signal to FF2 module
    Modified FF2 module to include enable signal and removed reset condition.

--- rtl/FF2.v
+++ rtl/FF2.v
@@ -1,16 +1,16 @@
 `timescale 1ns/1ps
 
-module FF2 (input ro_in, input clk, input rst_n, output reg ro_out );
+module FF2 (input ro_in, input clk, input en, output reg ro_out );
     
     reg f1;
-    always @( posedge clk or negedge rst_n ) begin
-        if (!rst_n) begin
-            f1 <= 1'b0;
-            ro_out <= 1'b0;
+    always @(posedge clk) begin
+        if (en) begin
+           f1 <= ro_in;
+           ro_out <= f1; 
         end
         else begin
-            f1 <= ro_in;
-            ro_out <= f1;
+            f1 <= 1'b0;
+            ro_out <= 1'b0;
         end
     end
 endmodule
