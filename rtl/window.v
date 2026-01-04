916cd5e0fee2fb861643bd4880158d9912e0bf7e
Rahul M <rahul735857@gmail.com>
2026-01-04 13:30:35 +0530

    Refactor window module to use enable signal

--- rtl/window.v
+++ rtl/window.v
@@ -1,18 +1,15 @@
 `timescale 1ns/1ps
 
-module window(input clk,input rst_n,output reg window_done );
+module window(input clk, input en, output reg window_done );
     
     parameter CLK_SPEED = 100_000_000;
     // parameter MS = 0.1;
     // parameter LIM = (MS*CLK_SPEED)/1000;
     parameter LIM = 10_000;
+    
     reg [31:0]ct;
-    always @( posedge clk or negedge rst_n) begin
-        if (!rst_n) begin
-            window_done <= 1'b0;
-            ct <= 32'b0;
-        end
-        else begin
+    always @(posedge clk) begin
+        if (en) begin
             if (ct == LIM-1) begin
                 window_done <= 1'b1;
                 ct <= 32'b0;
@@ -21,6 +18,10 @@ module window(input clk,input rst_n,output reg window_done );
                 ct <= ct + 1'b1;
                 window_done <= 1'b0; //deassert as it must be 1'b1 for only one clk pulse
             end
+        end
+        else begin
+            window_done <= 1'b0;
+            ct <= 32'b0;
         end     
     end
 endmodule
